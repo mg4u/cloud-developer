5f9dfb3ab6505570bca2690c461c61cedd666cc9
Mahmoud Gamal <mahmoudgz2003@gmail.com>
2020-02-23 14:20:03 +0200

    fix issues, and set the api id

--- course-04/client/src/config.ts
+++ course-04/client/src/config.ts
@@ -1,5 +1,5 @@
 // TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = 'b5gnuybotd'
+const apiId = '5cb0sr6e25'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
